3696ad8998ef234202d566682e14bf33bc60aa55
David Schneider <dnschneid@gmail.com>
2015-12-13 13:44:35 +0900

    Remove unused Util functions

--- src/Util.js
+++ src/Util.js
@@ -1,14 +1,4 @@
 var Util = {
-  // Properly converts YYYY-MM-DD to a Date object in the local timezone.
-  // (Why? new Date('YYYY-MM-DD') treats it as UTC.)
-  parseDate: function(input) {
-    var parts = input.split('-');
-    var year = parseInt(parts[0], 10);
-    var month = parseInt(parts[1], 10) - 1; // Note: month is 0-indexed
-    var day = parseInt(parts[2], 10);
-    return new Date(year, month, day);
-  },
-
   // '19:00'
   formatTime: function(date) {
     return date.toTimeString().substring(0, 5);
@@ -161,16 +151,6 @@ var Util = {
       TRASH: 8
     };
     return priorities[a.label.id] - priorities[b.label.id];
-  },
-  
-  isAllDayEvent: function(event) {
-    return !!event.start.date;
-  },
-  
-  getNextDate: function(date) {
-    var nextDate = new Date(date.toDateString());
-    nextDate.setDate(nextDate.getDate() + 1);
-    return nextDate;
   }
 };
 
